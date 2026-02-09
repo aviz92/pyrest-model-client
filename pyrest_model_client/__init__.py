6687b4b2946b7ca8dcced07f9396fae67d7e819e
avizaguri-macbook <avi210892@gmail.com>
2026-02-10 00:20:07 +0200

    Add get_model_fields to exports in __init__.py

--- pyrest_model_client/__init__.py
+++ pyrest_model_client/__init__.py
@@ -1,12 +1,13 @@
 from dotenv import load_dotenv
 
-from pyrest_model_client.base import BaseAPIModel
+from pyrest_model_client.base import BaseAPIModel, get_model_fields
 from pyrest_model_client.client import AsyncRequestClient, RequestClient, build_header
 
 load_dotenv()
 
 __all__ = [
     "BaseAPIModel",
+    "get_model_fields",
     "RequestClient",
     "AsyncRequestClient",
     "build_header",
